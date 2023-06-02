c0aecae43b54a15d34fd072263061fb33c27d6b0
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-06-02 22:34:38 +0600

    Use Axios Interceptor

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,22 +1,30 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
   const { user } = useContext(AuthContext);
-  const token = localStorage.getItem("access-token");
+  const [axiosSecure] = useAxiosSecure();
+
+  // const token = localStorage.getItem("access-token");
+
   const { refetch, data: cart = [] } = useQuery({
     queryKey: ["carts", user?.email],
+    // queryFn: async () => {
+    //   const response = await fetch(
+    //     `http://localhost:5000/carts?email=${user?.email}`,
+    //     {
+    //       headers: {
+    //         authorization: `bearer ${token}`,
+    //       },
+    //     }
+    //   );
+    //   return response.json();
+    // },
     queryFn: async () => {
-      const response = await fetch(
-        `http://localhost:5000/carts?email=${user?.email}`,
-        {
-          headers: {
-            authorization: `bearer ${token}`,
-          },
-        }
-      );
-      return response.json();
+      const response = await axiosSecure(`/carts?email=${user?.email}`);
+      return response.data;
     },
   });
   return [cart, refetch];
