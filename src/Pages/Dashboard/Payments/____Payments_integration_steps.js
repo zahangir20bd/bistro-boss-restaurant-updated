4703bd27ba7a92a5e49cf4e7327b221858fc0095
Zahangir Alam <eng.zahangir20bd@gmail.com>
2023-06-07 20:32:55 +0600

    Create Payment intents and use test card for test payments

--- src/Pages/Dashboard/Payments/____Payments_integration_steps.js
+++ src/Pages/Dashboard/Payments/____Payments_integration_steps.js
@@ -4,4 +4,8 @@
  * 3. Create Account on Stripe and get the publishable Key
  * 4. get Card information
  * 5. Create a payment Method
+ * 6. use test card to test payment
+ * 7. on the server side: install stripe: npm install --save  stripe
+ * 8. Create a payment intent api with payment method types: ["card"]
+ * 9. Make sure you provide amount in cents (multiply the price with 100)
  * */
